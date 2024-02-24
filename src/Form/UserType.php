683129cb6f6500fc037647b9ae8f2baba39c89e9
BenDejardin <73031539+BenDejardin@users.noreply.github.com>
2024-02-24 15:06:16 +0100

    Role assignment for users

---
+++ src/Form/UserType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('username', TextType::class, [
+            'label' => 'Nom d\'utilisateur',
+            'attr' => ['class' => 'form-control'],
+        ])
+        ->add('password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'invalid_message' => 'Les deux mots de passe doivent correspondre.',
+            'required' => true,
+            'first_options'  => [
+                'label' => 'Mot de passe',
+                'attr' => ['class' => 'form-control'],
+            ],
+            'second_options' => [
+                'label' => 'Tapez le mot de passe à nouveau',
+                'attr' => ['class' => 'form-control'],
+            ],
+        ])
+        ->add('email', EmailType::class, [
+            'label' => 'Adresse email',
+            'attr' => ['class' => 'form-control'],
+        ])
+        ->add('role', ChoiceType::class, [
+            'choices' => [
+                'Admin' => 'ROLE_ADMIN',
+                'User' => 'ROLE_USER',
+            ],
+            'label' => 'Role', 
+            'attr' => ['class' => 'form-select'],
+        ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
